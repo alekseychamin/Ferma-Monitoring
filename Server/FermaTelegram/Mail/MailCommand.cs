c0c3bb2ebf0e8b6327733766ad2f6fb419dba394
Aleksey <38381071+alekseychamin@users.noreply.github.com>
2018-04-18 18:41:57 +0300

    build and change alertemail

--- Server/FermaTelegram/Mail/MailCommand.cs
+++ Server/FermaTelegram/Mail/MailCommand.cs
@@ -24,7 +24,7 @@ namespace FermaTelegram
         private string filename;
         private string fermaEmailAddr = "fermaalnik@gmail.com";
         private List<string> fromEmailAddr = new List<string>();
-        private string alertEmailAddr = "aleksey.chamin@gmail.com";
+        private string alertEmailAddr = "ale-san2006@mail.ru";
         public ListMessage listMessage;
         public Task fetchMailCommand;
         public Task sendMailToClient;
